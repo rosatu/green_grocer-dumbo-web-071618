09827b8f78db62cc1a124f1c92e6b9c4b0e33ee6
Rosa Tu <rosalina.tu@gmail.com>
2018-07-15 18:17:34 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,17 @@
+require 'pry'
 def consolidate_cart(cart)
-  # code here
+  consolidated_groceries = {}
+  cart.each do |grocery_item_hash|
+      grocery_item.each do |item, price_hash|
+      binding.pry
+        if consolidated_groceries[p_c]== nil
+          consolidated_groceries[p_c]={price: amt_true[:price], clearance: amt_true[:clearance], count: 1}
+        else
+          consolidated_groceries[p_c][:count]+=1
+        end
+      end
+  end
+  return consolidated_groceries
 end
 
 def apply_coupons(cart, coupons)
